971dea819291989a39be3adc33b672fcc5d52e2c
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2025-01-14 14:06:13 +0300

    Add work with filtering and ordering

--- app/vps/views.py
+++ app/vps/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from vps.serializers import ChangeStatusSerializer, VPSSerializer
@@ -9,6 +11,8 @@ class VPSViewSet(ModelViewSet):
     queryset = VPS.objects.all()
     serializer_class = VPSSerializer
     http_method_names = ['get', 'post', 'patch']
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    filterset_fields = ('status',)
 
     def get_serializer_class(self):
         if self.action == 'partial_update':
