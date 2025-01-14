6e88c29e4cf0316adeafb0b441e655da0a3eef9c
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2025-01-14 14:49:13 +0300

    Add min and max values for VPS fields

--- app/vps/models.py
+++ app/vps/models.py
@@ -1,7 +1,8 @@
 from uuid import uuid4
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from vps.consts import SERVER_STATUSES
+from vps.consts import MAX_CPU, MAX_HDD, MAX_RAM, SERVER_STATUSES
 
 
 class VPS(models.Model):
@@ -13,9 +14,18 @@ class VPS(models.Model):
         default=uuid4,
         editable=False,
     )
-    cpu = models.PositiveIntegerField('количество процессорных ядер')
-    ram = models.PositiveIntegerField('объем оперативной памяти')
-    hdd = models.PositiveIntegerField('объем дискового пространства')
+    cpu = models.PositiveIntegerField(
+        'количество процессорных ядер',
+        validators=[MinValueValidator(1), MaxValueValidator(MAX_CPU)],
+    )
+    ram = models.PositiveIntegerField(
+        'объем оперативной памяти',
+        validators=[MinValueValidator(1), MaxValueValidator(MAX_RAM)],
+    )
+    hdd = models.PositiveIntegerField(
+        'объем дискового пространства',
+        validators=[MinValueValidator(1), MaxValueValidator(MAX_HDD)],
+    )
     status = models.CharField(
         'текущий статус сервера',
         max_length=16,
