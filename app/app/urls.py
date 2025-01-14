61bd7108017999e7e7f28b654336573ed4540b1c
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2025-01-14 14:51:22 +0300

    Add swagger doc

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,10 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="VPS API",
+        default_version='v1',
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('vps.urls'))
+    path('api/', include('vps.urls')),
+    path(
+        '',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
 ]
