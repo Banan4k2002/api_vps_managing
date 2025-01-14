98154d4725365b0c302fb2bdc68db0f065ed98c2
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2025-01-14 13:55:33 +0300

    Add endpoints for VPS

---
+++ app/vps/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from vps.models import VPS
+
+
+class VPSSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = VPS
+        fields = ('uid', 'cpu', 'ram', 'hdd', 'status')
+
+
+class ChangeStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = VPS
+        fields = ('status',)
--- app/vps/views.py
+++ app/vps/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from vps.serializers import ChangeStatusSerializer, VPSSerializer
+from vps.models import VPS
+
+
+class VPSViewSet(ModelViewSet):
+    """Работа с виртуальными серверами."""
+    queryset = VPS.objects.all()
+    serializer_class = VPSSerializer
+    http_method_names = ['get', 'post', 'patch']
+
+    def get_serializer_class(self):
+        if self.action == 'partial_update':
+            return ChangeStatusSerializer
+        return super().get_serializer_class()
